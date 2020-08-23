84949713f836c82c3008e2e92160e0364dafa7a7
Anonymous Ninja <anonymousninja.git@gmail.com>
2020-08-23 23:23:22 +1000

    Updates

---
+++ precache-manifest.0a3c5f9da5ad3f1b147225afab4e2401.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58251cca356bc3e469570d0828e31385",
+    "url": "/Covid19Australia/index.html"
+  },
+  {
+    "revision": "7724f972df26eb869f23",
+    "url": "/Covid19Australia/static/css/main.69ab46c1.chunk.css"
+  },
+  {
+    "revision": "d4e63780e8cd737af5f3",
+    "url": "/Covid19Australia/static/js/2.a7afe223.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid19Australia/static/js/2.a7afe223.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7724f972df26eb869f23",
+    "url": "/Covid19Australia/static/js/main.f0e83b76.chunk.js"
+  },
+  {
+    "revision": "4fe85060f5a46f56af9e",
+    "url": "/Covid19Australia/static/js/runtime-main.aa991221.js"
+  },
+  {
+    "revision": "02155d96e4a3f18305ab944925389c77",
+    "url": "/Covid19Australia/static/media/archia-regular-webfont.02155d96.woff2"
+  },
+  {
+    "revision": "a1d4030b69afb12540b19be94dcfba1e",
+    "url": "/Covid19Australia/static/media/au_map.a1d4030b.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Covid19Australia/static/media/react-logo.5d5d9eef.svg"
+  }
+]);
